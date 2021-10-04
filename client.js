b40a0548d84717e977b85c40d32dca6d41bd83fe
Eric Meinzer <emeinzer7@gmail.com>
2021-10-04 17:00:28 -0500

    wraped up for the day

--- client.js
+++ client.js
@@ -2,17 +2,46 @@ console.log('Here are all the available people:', people);
 
 $(readyNow)
 
-function readyNow(){
+let taco = []
+
+function readyNow() {
     console.log('jq connected');
     pictures();
+    $(`.image`).on(`click`, guess);
 }
-
 
 function pictures() {
 
+
     for (let pictures of people) {
-    $(`#imageContainer`).append(`<div >
-    <img src="https://github.com/${pictures.githubUsername}.png?size=250" alt="Profile image of ${pictures.name}">
-    </div>`)
+
+        storedPhotos = $(`<div>
+        <img class="image" src="https://github.com/${pictures.githubUsername}.png?size=250" alt="Profile image of ${pictures.name}">
+        </div>`)
+
+        storedPhotos.data('taco', pictures)
+        $(`#imageContainer`).append(storedPhotos)
+        console.log(storedPhotos.data());
+        taco.push(storedPhotos);
+
+    }
+
+}
+
+console.log(taco);
+
+
+
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function guess() {
+    console.log('GUESS');
+    if (people[randomNumber(0, people.length - 1)] === $(this)) {
+        return alert("CORRECT!!")
+    } else {
+        return alert("womp womp... try again!")
     }
 }
