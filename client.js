9dbac338e19375d5e92e27ffdbe3bea1ba9f332a
Eric Meinzer <emeinzer7@gmail.com>
2021-10-04 22:13:58 -0500

    added css, minor updates

--- client.js
+++ client.js
@@ -2,16 +2,19 @@ console.log('Here are all the available people:', people);
 
 $(readyNow)
 
-let taco = []
+let usernameList = []
+
+
 
 function readyNow() {
     console.log('jq connected');
-    pictures();
+    displayPictures();
     $(`.image`).on(`click`, guess);
 }
 
-function pictures() {
 
+
+function displayPictures() {
 
     for (let pictures of people) {
 
@@ -19,17 +22,18 @@ function pictures() {
         <img class="image" src="https://github.com/${pictures.githubUsername}.png?size=250" alt="Profile image of ${pictures.name}">
         </div>`)
 
-        storedPhotos.data('taco', pictures)
+        storedPhotos.data('list', pictures)
+
         $(`#imageContainer`).append(storedPhotos)
+
         console.log(storedPhotos.data());
-        taco.push(storedPhotos);
+
+        usernameList.push(storedPhotos);
 
     }
 
 }
-
-console.log(taco);
-
+console.log(usernameList);
 
 
 
@@ -37,11 +41,16 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
+$(`#displayText`).text(`yes`)
+
 function guess() {
-    console.log('GUESS');
-    if (people[randomNumber(0, people.length - 1)] === $(this)) {
+    console.log('clicked on image');
+    
+    if (people[randomNumber(0, people.length - 1)].name === clickedName) {
         return alert("CORRECT!!")
     } else {
         return alert("womp womp... try again!")
     }
 }
+
+
